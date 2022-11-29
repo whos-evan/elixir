38928dcb47d83cbaa5a3fdc484393d0f5553c125
Biune <you@example.com>
2022-11-28 21:56:26 -0600

    added footer, tos, pp, package.json, etc.

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,13 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: {
     relative: true,
-    files: ['./public/index.html']
+    files: ['./public/index.html', './public/about.html'],
   },
   theme: {
     extend: {
       fontFamily: {
-        'Inter': ['Inter', 'sans-serif'],
+        'Inter': ['Inter', 'sans-serif']
       },
       colors: {
         'primary': '#0d2c4c',
