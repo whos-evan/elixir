291f2738eec9ed47dff81de9d585ac83952b1d5c
Biune <you@example.com>
2022-11-29 18:27:39 -0600

    a

--- static/public/index.js
+++ static/public/index.js
@@ -274,3 +274,17 @@ function changeFavicon(src) {
     }
     document.head.appendChild(link);
    }
+
+// This variable will save the event for later use.
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevents the default mini-infobar or install dialog from appearing on mobile
+  e.preventDefault();
+  // Save the event because you'll need to trigger it later.
+  deferredPrompt = e;
+  // Show your customized install prompt for your PWA
+  // Your own UI doesn't have to be a single element, you
+  // can have buttons in different locations, or wait to prompt
+  // as part of a critical journey.
+  showInAppInstallPromotion();
+});
