82bb159c848f5a20a07090e0383b0798b4c9ddcb
Biune <you@example.com>
2022-12-02 17:35:07 -0600

    organization

--- static/public/js/register-uv.js
+++ static/public/js/register-uv.js
@@ -7,7 +7,7 @@ async function registerSW() {
   if (!navigator.serviceWorker)
     throw new Error("Your browser doesn't support service workers.");
 
-  await navigator.serviceWorker.register("./uv.js", {
+  await navigator.serviceWorker.register("../js/uv.js", {
     scope: __uv$config.prefix,
   });
 }
--- static/public/js/uv/uv.config.js
+++ static/public/js/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: '/js/uv/uv.handler.js',
+    bundle: '/js/uv/uv.bundle.js',
+    config: '/js/uv/uv.config.js',
+    sw: '/js/uv/uv.sw.js',
 };
