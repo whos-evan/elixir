3afc69be5a77a911f001ecbc9a67c91093aa8c40
Biune <you@example.com>
2022-12-03 00:18:13 -0600

    mb

--- static/public/js/index.js
+++ static/public/js/index.js
@@ -66,7 +66,7 @@ form.addEventListener("submit", async (event) => {
 // open url function
 function openURL(url) {
     window.navigator.serviceWorker
-    .register("./uv.js", {
+    .register("/js/uv.js", {
       scope: __uv$config.prefix,
     })
     .then(() => {
